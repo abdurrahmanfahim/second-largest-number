7841dac04854c8c63179e8f69b5495dfe100b9c9
Abdur Rahman Fahim <127137394+abdurrahmanfahim@users.noreply.github.com>
2025-01-19 22:46:42 +0600

    adding comments

--- js/script.js
+++ js/script.js
@@ -4,18 +4,24 @@
 // Example Output: 12
 
 
+// the array whome you want to filter this.
 const array = [10, 5, 20, 8, 12];
 
+// sorting that and returning this in result variable.
 let result = array.sort((a, b) => {
 
+    // returning with checking condition, array sorted decending.
     return(a - b);
 
 })
 
+
 console.log(result);
 
+// storing sorted array langth, the lenght is +1 form array index defaultly.
 let secondEnd = result.length
 
+// minus 1 for minus from length. another minus for the second largest number that sorted.
 console.log(result[secondEnd - 2]);
 
 
